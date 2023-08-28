d5ad0d80272dd8ac3585524a682a81df6e6089e0
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-08-28 15:40:29 +0800

    initial commit

---
+++ fullstackweather/src/main/java/sg/edu/nus/iss/fullstackweather/FullstackweatherApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.fullstackweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackweatherApplication.class, args);
+	}
+
+}
---
+++ fullstackweather/src/test/java/sg/edu/nus/iss/fullstackweather/FullstackweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.fullstackweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
